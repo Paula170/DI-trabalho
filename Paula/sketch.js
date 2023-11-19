1ffbdacef4dc6ab4b871724103823420024e7ea7
Paula170 <72363320+Paula170@users.noreply.github.com>
2023-11-19 21:40:35 +0000

    General menu sketch.js

--- Paula/sketch.js
+++ Paula/sketch.js
@@ -2,10 +2,29 @@ let planets = [];
 let backgroundColor = 0;
 let isBlue = true;
 
+let closeButtonSize = 20;
+let closeButtonX, closeButtonY;
+let bpmSlider, rockVolumeSlider, gasVolumeSlider;
+let menuVisible = false;
+
+
+let settingsIconX = 20;
+let settingsIconY = 20;
+let settingsIconSize = 30;
+
+
+let menuX;
+let menuY;
+let menuWidth = 400;
+let menuHeight = 200;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   const centerX = width / 2;
   const centerY = height / 2;
+
+  menuX = (width - menuWidth) / 2;
+  menuY = (height - menuHeight) / 2;
 
   planets = [
     new Planet(centerX, centerY, 150, color(255, 204, 0)),
@@ -18,6 +37,21 @@ function setup() {
     new Planet(centerX - 580, centerY - 400, 20, color(255, 255, 0)),
     new Planet(centerX + 660, centerY, 20, color(0, 255, 255)),
   ];
+
+
+  bpmSlider = createSlider(60, 180, 120); 
+  rockVolumeSlider = createSlider(0, 1, 0.5, 0.01); 
+  gasVolumeSlider = createSlider(0, 1, 0.5, 0.01);
+
+  
+  bpmSlider.position(menuX + 150, menuY + 45);
+  rockVolumeSlider.position(menuX + 150, menuY + 85);
+  gasVolumeSlider.position(menuX + 150, menuY + 125);
+
+  
+  bpmSlider.hide();
+  rockVolumeSlider.hide();
+  gasVolumeSlider.hide();
 }
 
 function draw() {
@@ -26,12 +60,79 @@ function draw() {
     planet.update();
     planet.display();
   });
+
+  
+  fill(255);
+  rect(settingsIconX, settingsIconY, settingsIconSize, settingsIconSize);
+  fill(0);
+  textSize(16);
+  textAlign(CENTER, CENTER);
+  text('⚙️', settingsIconX + settingsIconSize / 2, settingsIconY + settingsIconSize / 2);
+
+
+  if (menuVisible) {
+    fill(255);
+    rect(menuX, menuY, menuWidth, menuHeight);
+
+    fill(0);
+    textSize(16);
+    textAlign(LEFT, TOP);
+    text('BPM', menuX + 50, menuY + 45);
+    text('Rock Volume', menuX + 50, menuY + 85);
+    text('Gas Volume', menuX + 50, menuY + 125);
+
+    closeButtonX = menuX + menuWidth - closeButtonSize - 10;
+    closeButtonY = menuY + 10;
+
+ 
+    fill(255, 0, 0);
+    rect(closeButtonX, closeButtonY, closeButtonSize, closeButtonSize);
+    fill(255);
+    textSize(16);
+    textAlign(CENTER, CENTER);
+    text('X', closeButtonX + closeButtonSize / 2, closeButtonY + closeButtonSize / 2);
+
+
+
+  
+  
+    bpmSlider.show();
+    rockVolumeSlider.show();
+    gasVolumeSlider.show();
+
+    
+  } else {
+   
+    bpmSlider.hide();
+    rockVolumeSlider.hide();
+    gasVolumeSlider.hide();
+  }
 }
 
 function mousePressed() {
   planets.forEach(planet => {
     planet.startDragging();
   });
+
+  if (mouseX > settingsIconX && mouseX < settingsIconX + settingsIconSize &&
+    mouseY > settingsIconY && mouseY < settingsIconY + settingsIconSize) {
+  menuVisible = !menuVisible;
+}
+
+if (menuVisible && mouseX > closeButtonX && mouseX < closeButtonX + closeButtonSize &&
+    mouseY > closeButtonY && mouseY < closeButtonY + closeButtonSize) {
+  menuVisible = false;
+}
+
+ 
+  if (planets[0].contains(mouseX, mouseY)) {
+    if (isBlue) {
+      backgroundColor = color(40, 89, 137);
+    } else {
+      backgroundColor = color(47, 43, 69);
+    }
+    isBlue = !isBlue;
+  }
 }
 
 function mouseReleased() {
@@ -48,7 +149,7 @@ class Planet {
     this.planetColor = planetColor;
     this.dragging = false;
     this.angle = 0;
-    this.radius = dist(this.x, this.y, width / 2, height / 2); // Distancia al centro para movimiento circular
+    this.radius = dist(this.x, this.y, width / 2, height / 2);
   }
 
   startDragging() {
@@ -63,7 +164,7 @@ class Planet {
 
   update() {
     if (this.dragging) {
-      // Si está siendo arrastrado, actualiza la posición de manera circular
+      
       this.angle = atan2(mouseY - height / 2, mouseX - width / 2);
       this.x = width / 2 + this.radius * cos(this.angle);
       this.y = height / 2 + this.radius * sin(this.angle);
